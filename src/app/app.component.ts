fe15171dc5161ad22dc62b7388a682685aec56aa
kylemoranp@gmail.com <kylemoranp@gmail.com>
2020-01-25 13:22:51 -0500

    Alert a message when sign in is clicked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from "@angular/core";
   templateUrl: "./login/login.component.html",
   styleUrls: ["./login/login.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  email: string = "";
+
+  submit(): void{
+    const message: string = `You're using this email: ${this.email}`;
+    console.log(message);
+    alert(`You're using this email: ${this.email}`);
+  }
+  
+}
