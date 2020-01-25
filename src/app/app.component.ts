988df39469c194dba54a9f4eac9aff263241d6a2
kylemoranp@gmail.com <kylemoranp@gmail.com>
2020-01-25 12:58:22 -0500

    Inital commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "gr-login",
+  moduleId: module.id,
+  templateUrl: "./login/login.component.html",
+  styleUrls: ["./login/login.component.css"]
+})
+export class AppComponent {}
